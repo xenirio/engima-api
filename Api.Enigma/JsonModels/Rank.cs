47ca3e6117d9d4d8a9aac488f48de10a7267d0db
Vee <vee.jirapong@gmail.com>
2019-10-14 16:27:42 +0700

    Update score calculation

--- Api.Enigma/JsonModels/Rank.cs
+++ Api.Enigma/JsonModels/Rank.cs
@@ -5,6 +5,10 @@ namespace Api.Enigma.JsonModels
     {
         public string Player { get; set; }
 
-        public int Score { get; set; }
+        private decimal _score;
+        public decimal Score { 
+            get { return decimal.Round(_score, 2); }
+            set { _score = value; } 
+        }
     }
 }
--- Api.Enigma/Repositories/Entities/ScoreEntity.extend.cs
+++ Api.Enigma/Repositories/Entities/ScoreEntity.extend.cs
@@ -1,14 +1,16 @@
 ﻿using System;
+using System.Diagnostics;
+
 namespace Api.Enigma.Repositories.Entities
 {
     public partial class ScoreEntity
     {
-        public int Score
+        public decimal Score
         {
             get
             {
                 // score = (step / time) * 10
-                return (this.Step / this.Time) * this.Level * 10;
+                return (decimal.Divide(1.00m, Convert.ToDecimal(this.Step)) + decimal.Divide(1.00m,Convert.ToDecimal(this.Time))) * this.Level * 10;
             }
 
         }
