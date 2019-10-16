63c5b3a59e32361fbb1969064f6e277fc38142fd
sooparerk sakyai <sooparerk@mycostech.com>
2019-10-16 11:36:06 +0700

    Integrate swagger

--- Api.Enigma/Startup.cs
+++ Api.Enigma/Startup.cs
@@ -13,6 +13,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using MissionLib;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace Api.Enigma
 {
@@ -37,6 +38,13 @@ namespace Api.Enigma
                     .AllowAnyMethod()
                     .AllowAnyHeader();
             }));
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "Enigma Api Reference", Version = "v1" });
+            });
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
@@ -52,6 +60,16 @@ namespace Api.Enigma
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Enigma API V1");
+            });
+
             app.UseCors("MyPolicy");
             app.UseHttpsRedirection();
             app.UseMvc();
